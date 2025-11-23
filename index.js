6842a36311a6f17ddc272df33679250800416de4
Michael <olu21003@byui.edu>
2025-11-22 17:25:55 -0700

    Update pipeline and app files

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const messages = [
   { id: 1, text: "Welcome to the Bulletin Board!" },
   { id: 2, text: "Don't forget to take breaks!" },
-  { id: 3, text: "Docker makes deployment easier." }
+  { id: 3, text: "Docker makes deployment easier." },
 ];
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json(messages);
 });
 
